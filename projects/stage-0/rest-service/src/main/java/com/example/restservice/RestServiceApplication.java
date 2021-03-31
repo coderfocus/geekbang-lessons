d6b5e2490ee1649031b7d108e0b06d049f14642f
coderfocus <songwenjie830@gmail.com>
2021-03-31 17:22:29 +0800

    init: rest-service

---
+++ projects/stage-0/rest-service/src/main/java/com/example/restservice/RestServiceApplication.java
@@ -0,0 +1,33 @@
+package com.example.restservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class RestServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestServiceApplication.class, args);
+    }
+
+
+    @GetMapping("/hello/world")
+    public String helloworld(){
+        return "hello,world";
+    }
+
+    @PostMapping("/hello/world1")
+    public String helloworld1(){
+        return "hello,world1";
+    }
+
+    @PostMapping("/hello/world2")
+    public String helloworld2(@RequestBody String name){
+        return name;
+    }
+}
